da4e33882d2a65646a091614b3753aee0ccb1165
donp8on <paytonadonis@gmail.com>
2025-02-04 15:10:12 -0500

    feat: implemented GenerateNormalRandom given a standard deviation and mean

--- payton_supplementt_p1_2/payton_supplementt_p1_2.cs
+++ payton_supplementt_p1_2/payton_supplementt_p1_2.cs
@@ -6,7 +6,11 @@ namespace payton_supplementt_p1_2
     {
         public static double GenerateNormalRandom(double mean, double stdDev, Random random = null)
         {
-            throw new NotImplementedException();
+            random ??= new Random();
+            double u1 = 1.0 - random.NextDouble(); //uniform(0,1] random doubles
+            double u2 = 1.0 - random.NextDouble();
+            double normal = Math.Sqrt(-2.0 * Math.Log(u1)) * Math.Sin(2.0 * Math.PI * u2); //random normal(0,1)
+            return mean + stdDev * normal; //function returns random normal(mean,stdDev^2)
         }
 
         public static string GeneratePasswordString(int length, Random random = null)
