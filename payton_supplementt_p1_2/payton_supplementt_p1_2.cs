20898d9eefa2f2fa5dea5be9e3b4ec27a7ab4a68
donp8on <paytonadonis@gmail.com>
2025-02-04 14:55:10 -0500

    chore: declared methods for random functions number, password, and color

--- payton_supplementt_p1_2/payton_supplementt_p1_2.cs
+++ payton_supplementt_p1_2/payton_supplementt_p1_2.cs
@@ -1,7 +1,22 @@
-﻿namespace payton_supplementt_p1_2
+﻿using System.Text;
+
+namespace payton_supplementt_p1_2
 {
     public class payton_supplementt_p1_2
     {
+        public static double GenerateNormalRandom(double mean, double stdDev, Random random = null)
+        {
+            throw new NotImplementedException();
+        }
 
+        public static string GeneratePasswordString(int length, Random random = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static (string, (int, int, int)) GenerateRandomColor(Random random = null)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
