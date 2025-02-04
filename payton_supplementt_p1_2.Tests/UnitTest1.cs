1f76b090c4cfac3a9d20635392a739c2720a1c48
donp8on <paytonadonis@gmail.com>
2025-02-04 15:02:19 -0500

    test: added test for random number generator normal distrubition given standard deviation and mean

--- payton_supplementt_p1_2.Tests/UnitTest1.cs
+++ payton_supplementt_p1_2.Tests/UnitTest1.cs
@@ -3,9 +3,25 @@ namespace payton_supplementt_p1_2.Tests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void ShouldTestRandomNumberNormalDistribution()
         {
+            double mean = 50;
+            double stdDev = 10;
+            int sampleSize = 1000;
+            double sum = 0;
+            double sumSquares = 0;
 
+            for (int i = 0; i < sampleSize; i++)
+            {
+                double value = GenerateNormalRandom(mean, stdDev);
+                sum += value;
+                sumSquares += Math.Pow(value - mean, 2);
+            }
+
+            double actualMean = sum / sampleSize;
+            double actualStdDev = Math.Sqrt(sumSquares / sampleSize);
+
+            Assert.InRange(actualMean, mean - 1, mean + 1);
         }
     }
 }
